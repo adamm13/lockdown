a18f0f5a3e1c1877944250657e855ad467ff4741
Adam Mohammed <13.adamm@gmail.com>
2021-04-11 15:40:21 -0400

    added overworld maps

--- src/index.js
+++ src/index.js
@@ -2,21 +2,33 @@ import Phaser from "phaser";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
-import playGame from "./phaser/scene";
+import Town from "./phaser/scene";
 
 //console.log(App);
 
-export const config = {
-  type: Phaser.AUTO,
-  parent: "phaser",
-  width: 800,
-  height: 600,
-  scene: playGame
-};
 
+const config = {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  autoCenter: Phaser.Scale.CENTER_BOTH,
+  width: 800,
+  height: 640,
+  yoyo: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+
+  scene: [Town]
+};
 const game = new Phaser.Game(config);
+
+
 
 ReactDOM.render(
   <App />,
   document.getElementById("root") || document.createElement("div")
 );
+
