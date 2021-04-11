d53d11714c99a75b90a3f45c12475b1347093d6a
Caitie Garrood <caitlinanneg@gmail.com>
2021-04-11 18:05:48 -0400

    Moves player class out of scene files

--- src/phaser/overworld.js
+++ src/phaser/overworld.js
@@ -269,4 +269,47 @@ module.exports = { Town };
 //   }
 // }
 
+// class Town extends Phaser.Scene {
+//   constructor() {
+//     super("Town");
+//   }
+// // forest map
+// preload() {
+//   this.load.image('tiles', 'src/assets/town32.png');
+//   this.load.tilemapTiledJSON('map', 'src/assets/overworldv3.json');
+  
+//   this.load.spritesheet('player', 'src/assets/images/player.png', { frameWidth: 32, frameHeight: 32 });
+// }
+// create() {
+//   // environment
+  
+//   const map = this.make.tilemap({ key: 'map' });
+//   const tileset = map.addTilesetImage('town32', 'tiles')
+  
+//   const ground = map.createLayer("ground", tileset, 0, 0,);
+//   const house = map.createLayer("house", tileset, 0, 0);
+//   const trees = map.createLayer("trees", tileset, 0, 0);
+  
+  
+//   // camera
+//   this.cameras.main.setZoom(2);
+//   // Create player at start location and scale him
+//   this.player = new Player(this, 200, 300, 'player');
+//   const player = this.player;
+//   player.body.setCollideWorldBounds(true);
+
+//      //  Player physics properties.
+//      trees.setCollisionBetween(1, 1000);
+//      this.physics.add.collider(player, trees);
+ 
+//      house.setCollisionBetween(1, 200);
+//      this.physics.add.collider(player, house);
+//      player.body.setCollideWorldBounds(false);
+//    }
+//    update() {
+//      //  Input Events
+//      this.player.update();
+//    }
+//  }
+
 
