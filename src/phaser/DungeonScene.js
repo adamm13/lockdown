c458d7fb982109f7e8d368e240166db3f2aa1ee5
Adam Marsala <adammarsala@hotmail.com>
2021-04-12 15:03:45 -0400

    Make helper for rending objects in dungeon scene

--- src/phaser/DungeonScene.js
+++ src/phaser/DungeonScene.js
@@ -16,20 +16,27 @@ export default class Dungeon extends Phaser.Scene {
     //this.load.image("logo", logoImg);
     this.load.image('dungeonTiles', "src/assets/dungeonMaps/dungeon/tilesets/dungeon-tileset.png");
     this.load.image('obj-tiles', "src/assets/dungeonMaps/dungeon/tilesets/dungeon-objects.png");
-    this.load.tilemapTiledJSON('dungMap', "src/assets/dungeonMaps/dungeon/dungeon.json");
+    this.load.image('samples', "src/assets/symbols-and-items/sample.png");
+    this.load.image('chest', "src/assets/symbols-and-items/chest.png");
+    this.load.tilemapTiledJSON('dungMap', "src/assets/dungeonMaps/dungeon/dungeonMapWithObjects.json");
     this.load.spritesheet('player', "src/assets/characters/player.png", { frameWidth: gameTileSize, frameHeight: gameTileSize });
     this.load.spritesheet('zombie', "src/assets/characters/enemies/zombie1.png", { frameWidth: gameTileSize, frameHeight: gameTileSize });
   }
   
   create() {
-    // environment
+    // Render environment
     const map = this.make.tilemap({key:'dungMap'});
     const tileset = map.addTilesetImage('dungeon-tileset', 'dungeonTiles');
-    const objTileset = map.addTilesetImage('dungeon-objects', 'obj-tiles');
     const ground = map.createLayer("belowPlayer", tileset, 0, 0);
-    const obstacles = map.createLayer("world", tileset, 0, 0);
-    const dungObjs = map.createLayer("objectLayer", objTileset, 0, 0);
-
+    const obstacles = map.createLayer("walls", tileset, 0, 0);
+    
+    // Render objects
+    const samples = this.physics.add.staticGroup();
+    this.renderObjects(map, samples, 'samples', 0.7);
+    const chests = this.physics.add.staticGroup();
+    this.renderObjects(map, chests, 'chest', 0.7);
+    
+    
     // camera
     this.cameras.main.setZoom(2);
     // Create player at start location and scale him
@@ -38,9 +45,9 @@ export default class Dungeon extends Phaser.Scene {
     player.body.setCollideWorldBounds(true);
 
     // Create zombie and pass in player as last argument for a target
-    this.zombie = new Zombie(this, 752, 380, 'zombie', player);
-    const zombie = this.zombie;
-    zombie.body.setCollideWorldBounds(true);
+    // this.zombie = new Zombie(this, 752, 380, 'zombie', player);
+    // const zombie = this.zombie;
+    // zombie.body.setCollideWorldBounds(true);
 
     // Make camera stop at edge of map
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
@@ -50,22 +57,22 @@ export default class Dungeon extends Phaser.Scene {
 
     //  Player physics properties.
     obstacles.setCollisionBetween(0, 300);
-    dungObjs.setCollisionBetween(1, 400);
+    // dungObjs.setCollisionBetween(1, 400);
     this.physics.add.collider(player, obstacles);
-    this.physics.add.collider(zombie, obstacles);
+    // this.physics.add.collider(zombie, obstacles);
     
-    this.physics.add.collider(player, dungObjs, (player, tile) => {
-      if (tile.index === 228) {
-        tile.collisionCallback = (collidingPlayer, collidingTile) => {
-          console.log("Scene transition exit Dungeon");
-          this.scene.start('Town', { 
-            comingFrom: "Dungeon",
-            currentHealth: this.player.health
-           });
-          this.scene.stop('Dungeon');
-        }
-      }
-    });
+    // this.physics.add.collider(player, dungObjs, (player, tile) => {
+    //   if (tile.index === 228) {
+    //     tile.collisionCallback = (collidingPlayer, collidingTile) => {
+    //       console.log("Scene transition exit Dungeon");
+    //       this.scene.start('Town', { 
+    //         comingFrom: "Dungeon",
+    //         currentHealth: this.player.health
+    //        });
+    //       this.scene.stop('Dungeon');
+    //     }
+    //   }
+    // });
 
 
     // Testing React State with zombieHit
@@ -77,7 +84,15 @@ export default class Dungeon extends Phaser.Scene {
   update() {
     //  Input Events
     this.player.update();
-    this.zombie.update();
+    // this.zombie.update();
+  }
+
+  // Custom Helpers
+  renderObjects(map, staticGroup, objString, scale) {
+    let newLayer = map.getObjectLayer(objString)["objects"];
+    newLayer.forEach(obj => {
+      let object = staticGroup.create(obj.x + 16, obj.y - 16, objString).setScale(scale);
+    });
   }
 
   zombieHit() {
@@ -85,6 +100,7 @@ export default class Dungeon extends Phaser.Scene {
     // this.player.health -= 1; 
     // console.log("Health: ", this.player.health);
   }
+
 
 }
 
