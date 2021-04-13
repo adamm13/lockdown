d7ebf663c68a659404aae294241d639dec005803
Adam Mohammed <13.adamm@gmail.com>
2021-04-12 23:40:19 -0400

    added menu scene

--- src/phaser/overworld.js
+++ src/phaser/overworld.js
@@ -43,7 +43,7 @@ class Town extends Phaser.Scene {
     const trees = map.createLayer("trees", tileset, 0, 0);
 
     // camera
-    this.cameras.main.setZoom(1);
+    this.cameras.main.setZoom(1.4);
 
     // Create player at start location and scale him
     this.player = new Player(this, 1000, 300, 'player');
