023fda966fbeb708acc2b7d9ef649206329c60e7
Adam Mohammed <13.adamm@gmail.com>
2021-04-15 21:42:44 -0400

    removed girl

--- src/phaser/testnpcanims.js
+++ src/phaser/testnpcanims.js
@@ -4,28 +4,28 @@ const createNpcAnims = (anims = Phaser.Animations.AnimationManager) => {
 
       anims.create({
           key: "walk1",
-          frames: anims.generateFrameNames("boy1", {start: 0, end: 2, prefix: "0", suffix: ".png"}),
+          frames: anims.generateFrameNames("boy1" && "girl1", {start: 0, end: 2, prefix: "0", suffix: ".png"}),
           repeat: -1,
           frameRate: 10
       }),
 
       anims.create({
           key: "left1",
-          frames: anims.generateFrameNames("boy1", {start: 3, end: 5, prefix: "0", suffix: ".png"}),
+          frames: anims.generateFrameNames("boy1" && "girl1", {start: 3, end: 5, prefix: "0", suffix: ".png"}),
           repeat: -1,
           frameRate: 10
       }),
 
       anims.create({
           key: "right1",
-          frames: anims.generateFrameNames("boy1", {start: 6, end: 8, prefix: "0", suffix: ".png"}),
+          frames: anims.generateFrameNames("boy1" && "girl1", {start: 6, end: 8, prefix: "0", suffix: ".png"}),
           repeat: -1,
           frameRate: 10
       }),
 
       anims.create({
           key: "back1",
-          frames: anims.generateFrameNames("boy1", {start: 9, end: 11, prefix: "0", suffix: ".png"}),
+          frames: anims.generateFrameNames("boy1" && "girl1", {start: 9, end: 11, prefix: "0", suffix: ".png"}),
           repeat: -1,
           frameRate: 10
       })
