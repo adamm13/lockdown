8e04f439f60060d35ca751fc0db1cd29672736d4
Adam Marsala <adammarsala@hotmail.com>
2021-04-17 21:09:14 -0400

    conditional chest renders after defeating all zombies in boss room

--- src/phaser/helpers/zombieHelpers.js
+++ src/phaser/helpers/zombieHelpers.js
@@ -15,14 +15,16 @@ const zombieFactory = (scene, zombieArray, spritesheetKey, target, obstacles) =>
   });
 };
 
-const zombieDamage = (zombie, shot, scene) => {
+const zombieDamage = (zombie, shot, scene, map, tileset, player) => {
+  console.log("Map: ", map);
   if(zombie.zombieData.health === 0){
     zombie.setVisible(false);
-    zombie.body.enable = false; 
+    zombie.body.enable = false;
+    // increment player kill count?
+    killZombie(zombie, scene, map, tileset, player);
   } else {
     zombie.tint = Math.random() * 0xffffff;
     zombie.zombieData.health -= 1;
-
     const directionX = zombie.x - shot.x;
     const directionY = zombie.y - shot.y;
     const direction = new Phaser.Math.Vector2(directionX, directionY).normalize().scale(200);
@@ -44,6 +46,30 @@ const zombieHit = (player, zombie) => {
 
   sceneEvents.emit('zombieHit', player.gameData.health);
   //this.sound.play("blood") // throttle this sound to play once/second
+};
+
+const killZombie = (zombie, scene, map, tileset, player) => {
+  // Remove zombie from scene zombies array
+  const zombieIndex = scene.zombies.indexOf(zombie); 
+  scene.zombies.splice(zombieIndex, 1); 
+  console.log(scene.zombies);
+  const finalScene = scene.scene.get('FinalBoss');
+  if (finalScene.zombies.length === 0) {
+    console.log("YOU WIN!");
+    renderChest(scene, map, tileset, player);
+  }
+}
+
+const renderChest = (scene, map, tileset, player) => {
+  const chest = map.createLayer("Chest", tileset, 0, 0);
+  chest.setCollisionBetween(0, 500);
+  scene.physics.add.collider(chest, player, (player, tile) => { 
+    getAntidote();
+  });
+}
+
+const getAntidote = () => {
+  console.log("YOU GOT THE ANTIDOTE");
 };
 
 module.exports = { zombieFactory, zombieDamage, zombieHit };
