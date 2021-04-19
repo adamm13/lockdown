c103e42a539e62a6d81ff90d84f8c3a2fb1679be
Adam Marsala <adammarsala@hotmail.com>
2021-04-19 12:25:42 -0400

    Remove tile collision logic for zombies

--- src/phaser/Zombie.js
+++ src/phaser/Zombie.js
@@ -1,23 +1,8 @@
 import { Entity } from "./Entity";
 
 let zombieShot = 0;
-const UP = 0
-const DOWN = 1
-const LEFT = 2
-const RIGHT = 3
-
-const randomDirection = (exclude = this.private_direction) => {
-  let newDirection = Phaser.Math.Between(0,3)
-  while (newDirection === exclude) {
-    newDirection = Phaser.Math.Between(0,3)
-  }
-  return newDirection
-}
-
 
 class Zombie extends Entity {
-
-  direction = DOWN
 
   constructor(scene, x, y, textureKey, target, speed, health) {
     super(scene, x, y, textureKey);
@@ -39,9 +24,6 @@ class Zombie extends Entity {
     const animFrameRate = 10;
     const anims = scene.anims;
     const spriteKey = this.textureKey; 
-
-    // handle tile collision
-    this.scene.physics.world.on(Phaser.Physics.Arcade.Events.TILE_COLLIDE, this.handleTileCollision, this, this.player)
 
     anims.create({
       key: spriteKey + '-left',
@@ -89,37 +71,6 @@ class Zombie extends Entity {
   bounceBack(direction){
     this.body.setVelocity(direction.x, direction.y);
     zombieShot = 1;
-  }
-
-  preUpdate(t, dt) {
-    super.preUpdate(t,dt)
-    
-    const speed = 120
-
-    switch (this.private_direction) {
-      case UP:
-        this.setVelocity(0, -speed) && this.anims.play(this.textureKey + '-up', this) 
-        break
-
-      case DOWN:
-        this.setVelocity(0, speed) && this.anims.play(this.textureKey + '-down', this)
-        break
-
-      case LEFT:
-        this.setVelocity(-speed, 0) && this.anims.play(this.textureKey + '-left', this)
-        break
-
-      case RIGHT:
-        this.setVelocity(speed, 0) && this.anims.play(this.textureKey + '-right', this)
-        break
-    }
-  }
-
-  handleTileCollision(go = Phaser.GameObjects.GameObject, tile = Phaser.Tile, player = this.player) {
-    if (go !== this || this.player) {
-      return
-    }
-    this.private_direction = randomDirection(this.private_direction)
   }
 
   update() {
