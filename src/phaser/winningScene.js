bed0f69cac7215f3f0eb993d74fadfbb6ed69dc9
Caitie Garrood <caitlinanneg@gmail.com>
2021-04-19 12:59:05 -0400

    Updates font style for winning scene

--- src/phaser/winningScene.js
+++ src/phaser/winningScene.js
@@ -18,9 +18,9 @@ You did it! Humanity is saved!`
 
 
 const textStyle = {
-  fontSize: 20,
+  fontSize: 25,
   lineSpacing:0,
-  fontFamily: "verdana",
+  fontFamily: "VT323",
   color: "WHITE",
 };
 
@@ -44,74 +44,89 @@ const Winning = new Phaser.Class({
 
       // camera transition effect
       this.cameras.main.fadeIn(5000);
-
       this.add.image(0, 0, "title_bg").setOrigin(0, 0).setVisible(!debug);
 
-      this.add.text(32, 32, win_game, textStyle).setAlpha(0.25).setVisible(debug);
+      //load fonts
+      let add = this.add;
+      let tweens = this.tweens;
+      let input = this.input;
+      let renderer = this.renderer;
+      let scene = this.scene;
 
-      const text = this.add.text(32, 32, win_game, textStyle);
-      const {
-        lineHeight,
-        lineSpacing,
-        lineWidths
-      } = Phaser.GameObjects.GetTextSize(
-        text,
-        text.getTextMetrics(),
-        win_game.split("\n")
-      );
-      const totalLineHeight = lineHeight + lineSpacing;
-      this.add
-        .grid(
-          text.x,
-          text.y,
-          text.width,
-          text.height,
-          lineHeight,
-          totalLineHeight,
-          0,
-          0,
-          0x00ffff,
-          0.2
-        )
-        .setOrigin(0, 0)
-        .setVisible(debug);
-      const maskImage = this.add
-        .graphics({
-          fillStyle: { color: 0xff0000, alpha: 0.5 }
-        })
-        .setVisible(debug);
-      const mask = maskImage.createGeometryMask();
+      WebFont.load({
+        google: {
+            families: [ 'VT323' ]
+        },
+        active: function ()
+        {
 
-      text.setMask(mask);
+          add.text(32, 32, win_game, textStyle).setAlpha(0.25).setVisible(debug);
 
-      const path = new Phaser.Curves.Path();
+          const text = add.text(32, 32, win_game, textStyle);
+          const {
+            lineHeight,
+            lineSpacing,
+            lineWidths
+          } = Phaser.GameObjects.GetTextSize(
+            text,
+            text.getTextMetrics(),
+            win_game.split("\n")
+          );
+          const totalLineHeight = lineHeight + lineSpacing;
+          this.add
+            .grid(
+              text.x,
+              text.y,
+              text.width,
+              text.height,
+              lineHeight,
+              totalLineHeight,
+              0,
+              0,
+              0x00ffff,
+              0.2
+            )
+            .setOrigin(0, 0)
+            .setVisible(debug);
+          const maskImage = add
+            .graphics({
+              fillStyle: { color: 0xff0000, alpha: 0.5 }
+            })
+            .setVisible(debug);
+          const mask = maskImage.createGeometryMask();
 
-      for (let i = 0, len = lineWidths.length; i < len; i++) {
-        const lineWidth = lineWidths[i];
-        const y = text.y + i * totalLineHeight;
+          text.setMask(mask);
 
-        path.moveTo(text.x, y).lineTo(text.x + lineWidth, y);
-      }
+          const path = new Phaser.Curves.Path();
 
-      const pathDisplay = this.add
-        .graphics({ lineStyle: { color: 0xffff00, alpha: 0.5, width: 2 } })
-        .setVisible(debug);
+          for (let i = 0, len = lineWidths.length; i < len; i++) {
+            const lineWidth = lineWidths[i];
+            const y = text.y + i * totalLineHeight;
 
-      path.draw(pathDisplay);
-
-      this.tweens.addCounter({
-        from: 0,
-        to: 1,
-        duration: 40 * win_game.length,
-        onUpdate: (counter) => {
-          const { x, y } = path.getPoint(counter.getValue());
-
-          maskImage.clear();
-          if (y > 0) {
-            maskImage.fillRect(text.x, text.y, text.width, y - text.y);
+            path.moveTo(text.x, y).lineTo(text.x + lineWidth, y);
           }
-          maskImage.fillRect(text.x, y, x - text.x, totalLineHeight);
-    }
+
+          const pathDisplay = add
+            .graphics({ lineStyle: { color: 0xffff00, alpha: 0.5, width: 2 } })
+            .setVisible(debug);
+
+          path.draw(pathDisplay);
+
+          tweens.addCounter({
+            from: 0,
+            to: 1,
+            duration: 40 * win_game.length,
+            onUpdate: (counter) => {
+              const { x, y } = path.getPoint(counter.getValue());
+
+              maskImage.clear();
+              if (y > 0) {
+                maskImage.fillRect(text.x, text.y, text.width, y - text.y);
+              }
+              maskImage.fillRect(text.x, y, x - text.x, totalLineHeight);
+            }
+          }); 
+        }
   });
 
         this.scene.stop("GameUI")
