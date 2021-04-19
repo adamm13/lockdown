386c8aa45d3857e45858debd338d2c4cc15b7507
Caitie Garrood <caitlinanneg@gmail.com>
2021-04-19 11:58:24 -0400

    Gets font loaded properly into intro

--- src/phaser/intro.js
+++ src/phaser/intro.js
@@ -25,7 +25,7 @@ Press ENTER to begin your nightmare......`
 const textStyle = {
   fontSize: 20,
   lineSpacing:0,
-  fontFamily: 'Special Elite',
+  fontFamily: 'VT323',
   color: "WHITE",
 };
 
@@ -39,7 +39,7 @@ const Intro = new Phaser.Class({
     {
         Phaser.Scene.call(this, 'Intro');
     },
-
+ 
     preload() 
     {
       this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
@@ -50,115 +50,116 @@ const Intro = new Phaser.Class({
         //       this.cameras.main.once('camerafadeincomplete', function (camera) {
         //     camera.fadeOut(6000);
         // });
-      //load fonts
-      WebFont.load({
-        google: {
-            families: [ 'Press Start 2P', 'Special Elite' ]
-        },
-        active: function() {
-          game.time.events.add(Phaser.Timer.SECOND, createText, this);
-           },
-      });  
 
       // camera transition effect
       this.cameras.main.fadeIn(5000);
-
       this.add.image(0, 0, "title_bg").setOrigin(0, 0).setVisible(!debug);
 
-      this.add.text(32, 32, game_intro, textStyle).setAlpha(0.25).setVisible(debug);
+      //load fonts
+      let add = this.add;
+      let tweens = this.tweens;
+  
+      WebFont.load({
+          google: {
+              families: [ 'Special Elite', 'VT323', 'Nosifer' ]
+          },
+          active: function ()
+          {
+            add.text(32, 32, game_intro, textStyle).setAlpha(0.25).setVisible(debug);
+        
 
-      const text = this.add.text(32, 32, game_intro, textStyle);
-      const {
-        lineHeight,
-        lineSpacing,
-        lineWidths
-      } = Phaser.GameObjects.GetTextSize(
-        text,
-        text.getTextMetrics(),
-        game_intro.split("\n")
-      );
-      const totalLineHeight = lineHeight + lineSpacing;
-      this.add
-        .grid(
-          text.x,
-          text.y,
-          text.width,
-          text.height,
-          lineHeight,
-          totalLineHeight,
-          0,
-          0,
-          0x00ffff,
-          0.2
-        )
-        .setOrigin(0, 0)
-        .setVisible(debug);
-      const maskImage = this.add
-        .graphics({
-          fillStyle: { color: 0xff0000, alpha: 0.5 }
-        })
-        .setVisible(debug);
-      const mask = maskImage.createGeometryMask();
-
-      text.setMask(mask);
-
-      const path = new Phaser.Curves.Path();
-
-      for (let i = 0, len = lineWidths.length; i < len; i++) {
-        const lineWidth = lineWidths[i];
-        const y = text.y + i * totalLineHeight;
-
-        path.moveTo(text.x, y).lineTo(text.x + lineWidth, y);
-      }
-
-      const pathDisplay = this.add
-        .graphics({ lineStyle: { color: 0xffff00, alpha: 0.5, width: 2 } })
-        .setVisible(debug);
-
-      path.draw(pathDisplay);
-
-      this.tweens.addCounter({
-        from: 0,
-        to: 1,
-        duration: 40 * game_intro.length,
-        onUpdate: (counter) => {
-          const { x, y } = path.getPoint(counter.getValue());
-
-          maskImage.clear();
-
-          if (y > 0) {
-            maskImage.fillRect(text.x, text.y, text.width, y - text.y);
+            const text = add.text(32, 32, game_intro, textStyle);
+            const {
+              lineHeight,
+              lineSpacing,
+              lineWidths
+            } = Phaser.GameObjects.GetTextSize(
+              text,
+              text.getTextMetrics(),
+              game_intro.split("\n")
+            );
+            const totalLineHeight = lineHeight + lineSpacing;
+            add
+              .grid(
+                text.x,
+                text.y,
+                text.width,
+                text.height,
+                lineHeight,
+                totalLineHeight,
+                0,
+                0,
+                0x00ffff,
+                0.2
+              )
+              .setOrigin(0, 0)
+              .setVisible(debug);
+            const maskImage = add
+              .graphics({
+                fillStyle: { color: 0xff0000, alpha: 0.5 }
+              })
+              .setVisible(debug);
+            const mask = maskImage.createGeometryMask();
+      
+            text.setMask(mask);
+      
+            const path = new Phaser.Curves.Path();
+      
+            for (let i = 0, len = lineWidths.length; i < len; i++) {
+              const lineWidth = lineWidths[i];
+              const y = text.y + i * totalLineHeight;
+      
+              path.moveTo(text.x, y).lineTo(text.x + lineWidth, y);
+            }
+      
+            const pathDisplay = add
+              .graphics({ lineStyle: { color: 0xffff00, alpha: 0.5, width: 2 } })
+              .setVisible(debug);
+      
+            path.draw(pathDisplay);
+      
+            tweens.addCounter({
+              from: 0,
+              to: 1,
+              duration: 40 * game_intro.length,
+              onUpdate: (counter) => {
+                const { x, y } = path.getPoint(counter.getValue());
+      
+                maskImage.clear();
+      
+                if (y > 0) {
+                  maskImage.fillRect(text.x, text.y, text.width, y - text.y);
+                }
+      
+                maskImage.fillRect(text.x, y, x - text.x, totalLineHeight);
           }
-
-          maskImage.fillRect(text.x, y, x - text.x, totalLineHeight);
-    }
-
-    
-  });
-        this.input.keyboard.once('keyup-ENTER', function () {
-
-          // Suppress WebGL warnings before changing scenes
-          text.texture = this.renderer.blankTexture; // Should be wrapped in conditional when rendering direct to canvas?
+      
           
-          // When starting the game fresh, we use this initial state
-          const data = {
-            comingFrom: "Intro",  
-            health: 500,
-            inventory: [],
-            sampleLocations: {
+        });
+          }
+      }); 
+      
+      this.input.keyboard.once('keyup-ENTER', function () {
+
+        // Suppress WebGL warnings before changing scenes
+        text.texture = this.renderer.blankTexture; // Should be wrapped in conditional when rendering direct to canvas?
+          
+        // When starting the game fresh, we use this initial state
+        const data = {
+          comingFrom: "Intro",  
+          health: 500,
+          inventory: [],
+          sampleLocations: {
               "Dungeon": null,
               "Town": null,
               "Forest": null
             }
-          }
+        }
           
-          this.scene.start('Town', data);
-          this.scene.stop('Intro');
+        this.scene.start('Town', data);
+        this.scene.stop('Intro');
       }, this);
-}
-
-
-    
+    }
 });
 
 module.exports = { Intro };
